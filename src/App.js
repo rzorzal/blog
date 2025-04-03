829b5736b9f946a758b34aab6e8e3fb3e81627ff
Ricardo Zorzal <ricardozorzal@hotmail.com>
2025-04-03 00:29:59 -0300

    log

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-
+    console.log("Test");
     this.selectIssue = this.selectIssue.bind(this);
     this.onSelectedIssue = this.onSelectedIssue.bind(this);
   }
